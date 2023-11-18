745135d9e5e2f50f31f145eb2d1ecee7d7e07242
AnshhSingh <anshsingh25bd@gmail.com>
2023-11-19 00:16:18 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
+const withMDX = require('@next/mdx')()
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
 module.exports = nextConfig
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'www.w3schools.com',
+          port: '',
+        //   pathname: '/account123/**',
+        },
+      ],
+    },
+  }
