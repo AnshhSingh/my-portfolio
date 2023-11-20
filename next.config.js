db7d976db97bf1662a5506538990dbd9b4f6651f
AnshhSingh <anshsingh25bd@gmail.com>
2023-11-20 17:17:14 +0530

    working with mdx

--- next.config.js
+++ next.config.js
@@ -1,19 +1,20 @@
 const withMDX = require('@next/mdx')()
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
 
-module.exports = nextConfig
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
 
-module.exports = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'www.w3schools.com',
-          port: '',
-        //   pathname: '/account123/**',
-        },
-      ],
-    },
-  }
+}
+// module.exports = {
+//     images: {
+//       remotePatterns: [
+//         {
+//           protocol: 'https',
+//           hostname: 'www.w3schools.com',
+//           port: '',
+//         //   pathname: '/account123/**',
+//         },
+//       ],
+//     },
+//   }
   module.exports = withMDX(nextConfig)
